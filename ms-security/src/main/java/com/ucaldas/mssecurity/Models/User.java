fc48b494e0f4e3938e4949b0a08b4af298cf2539
JuanGonzalezx <juandavidocampogonzalezjdo@gmail.com>
2024-04-09 17:06:23 -0500

    Implementacion microservicio seguridad

---
+++ ms-security/src/main/java/com/ucaldas/mssecurity/Models/User.java
@@ -0,0 +1,84 @@
+package com.ucaldas.mssecurity.Models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Data
+@Document
+public class User {
+    @Id
+    private String _id;
+    private String name;
+    private String email;
+    private String password;
+    private String token;
+    @DBRef
+    private Role role;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.token = null;
+    }
+
+
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @JsonIgnore
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
